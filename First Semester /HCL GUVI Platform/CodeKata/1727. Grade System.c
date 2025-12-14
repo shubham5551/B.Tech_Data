05c2c5304c7f659dc6ebc3ed558512c994032425
Shubham <58857424+shubham5551@users.noreply.github.com>
2025-12-14 22:19:12 +0530

    Add grade system program in C

---
+++ First Semester /HCL GUVI Platform/CodeKata/1727. Grade System.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    int marks;
+    scanf("%d", &marks);
+    if (marks >= 90)
+        printf("A");
+    else if (marks >= 75)
+        printf("B");
+    else if (marks >= 50)
+        printf("C");
+    else
+        printf("Fail");
+    return 0;
+}
